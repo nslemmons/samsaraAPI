dda04ff777f10862335436ec34c4d8f5a38cbae3
nslemmons <59349099+nslemmons@users.noreply.github.com>
2021-05-07 14:06:26 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/samsara', methods=['POST'])
+@app.route('/samsara', methods=['POST', 'GET'])
 def samsara():
     return render_template('samsara.html')
 
@@ -18,4 +18,4 @@ def example():
     return render_template('example.html')
 
 if __name__ == ('__main__'):
-    app.run(host='0.0.0.0',debug=True)
+    app.run(debug=True)
