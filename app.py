6d74cd22fa635731caa131f07f0b13d7f84f5591
nslemmons <59349099+nslemmons@users.noreply.github.com>
2021-05-07 13:47:15 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
@@ -6,13 +8,28 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/samsara')
+
+@app.route('/samsara, methods=[POST]')
 def samsara():
-    return render_template('samsara.html')
+
+    var = request.form['POST']
+
+    url = "https://api.samsara.com/fleet/drivers"
+
+    headers = {"Accept": "application/json","Authorization": ""}
+
+    headers.update(Authorization="Bearer "+var)
+
+    params = {'driverActivationStatus':'active'}
+
+    response = requests.request("GET", url, headers=headers, params=params).json()
+    
+    return render_template('samsara.html', response)
+    
 
 @app.route('/example')
 def example():
     return render_template('example.html')
 
 if __name__ == ('__main__'):
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
